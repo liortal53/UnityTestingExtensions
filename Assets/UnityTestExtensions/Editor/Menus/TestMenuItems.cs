fe97035d19b9a5c3e492f00773e2ddafff7283de
Lior Tal <liortal53@gmail.com>
2019-11-09 00:11:41 +0200

    Added TestMenuItems with static methods to launch edit mode or play mode tests.
    This method can also be invoked from the command line, or as part of a build script that builds a player to programmatically run tests.

---
+++ Assets/UnityTestExtensions/Editor/Menus/TestMenuItems.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+
+namespace UnityTestExtensions
+{
+    public static class TestMenuItems
+    {
+        /// <summary>
+        /// Runs all edit mode tests.
+        /// </summary>
+        [MenuItem("Window/General/Test Extensions/Run Edit Mode Tests")]
+        private static void RunEditModeTests()
+        {
+            RunTests(TestMode.EditMode);
+        }
+        
+        /// <summary>
+        /// Runs all play mode tests.
+        /// </summary>
+        [MenuItem("Window/General/Test Extensions/Run Play Mode Tests")]
+        private static void RunPlayModeTests()
+        {
+            RunTests(TestMode.PlayMode);
+        }
+
+        private static void RunTests(TestMode testModeToRun)
+        {
+            var testRunnerApi = ScriptableObject.CreateInstance<TestRunnerApi>();
+            var filter = new Filter()
+            {
+                testMode = testModeToRun
+            };
+            
+            testRunnerApi.Execute(new ExecutionSettings(filter));
+        }
+    }
+}
